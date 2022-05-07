1cb0b9625533db77e239d4ced909a05dfb07387c
Todd Brannon <toddqbrannon@gmail.com>
2022-05-07 13:07:02 -0500

    added queries and QueryExec and removed old copies of dbService and cleaned up unused lines of code in app.js, dbService.js and index.router.js

--- api/index/index.router.js
+++ api/index/index.router.js
@@ -1,20 +1,7 @@
 const express = require('express');
-var axios = require('axios');
-//var data = JSON.stringify({"first_name":"Todd","last_name":"Brannon","username":"tbrannon","email_address":"todd@nowhere.com","password":"password1234"});
-const prettyjson = require('prettyjson');
 const router = express.Router();
 
-var request = require('request');
-
-const database = require("../../config/database");
-// const pool = require("../../config/database");
-const sql2 = require('../../config/database');
-const database2 = require("../../config/database2");
-const res = require('express/lib/response');
-
 const dbService = require('../../dbService');
-
-
 
 router.get('/getAll', (request, response) => {
     const db = dbService.getDbServiceInstance();
@@ -25,25 +12,5 @@ router.get('/getAll', (request, response) => {
     .then(data => response.json({ data : data }))
     .catch(err => console.log(err));
 });
-
-
-
-router.get('/getArray', (request, response) => {
-    const db = dbService.getDbServiceInstance();
-    const result = db.getArray();
-    result
-    .then(data => response.json({ data : data }))
-    .catch(err => console.log(err));
-});
-
-router.get('/getEmails', (request, response) => {
-    const db = dbService.getDbServiceInstance();
-    const result = db.pushEmails();
-    result
-    .then(data => response.json({ data : data }))
-    .catch(err => console.log(err));
-})
-
-
 
 module.exports = router;
--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mysql = require('mysql');
 const ejs = require('ejs');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+
 require('dotenv').config();
 
 const http = require('http')
@@ -12,7 +14,6 @@ const port = process.env.PORT;
 // Requiring Routes
 // const indexRouter = require('./api/index/index.router_async_test')
 const indexRouter = require('./api/index/index.router')
-const enrollmentsRouter = require('./api/enrollments/enrollments.router')
 
 const app = express();
 
@@ -31,10 +32,35 @@ app.use(methodOverride("_method"));
 
 // Using Routes ==========================================
 app.use('/', indexRouter)
-app.use('/request', indexRouter)
-app.use('/enrollments', enrollmentsRouter)
 
 
+const QueryExec = require('./QueryExec')
+
+const Query01 = require('./queries/query01')
+const Query02 = require('./queries/query02')
+const Query03 = require('./queries/query03')
+const Query04a = require('./queries/query04a')
+const Query04c = require('./queries/query04c')
+const Query04d = require('./queries/query04d')
+const Query04f = require('./queries/query04f')
+const Query05 = require('./queries/query05')
+const Query06 = require('./queries/query06')
+//-----------
+let instance = null;
+dotenv.config();
+
+//array with all queries
+const queries = [
+    new Query01(),
+     new Query02(),
+     new Query03(),
+     new Query04a(),
+     new Query04c(),
+     new Query04d(),
+     new Query04f(),
+     new Query05(),
+     new Query06()
+]
 
 // listen for requests
 app.listen(process.env.PORT, () => {
